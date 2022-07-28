38ea998af26247a585f06fbdb809403347f90b28
Andreea Gotea <goteaandreea@gmail.com>
2022-07-28 15:30:53 +0300

    Test

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello" + "\n" + "Andreea");
+        System.out.println();
+    }
+}
+
